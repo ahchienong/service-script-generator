ce0ae0ab8db5ea73c9032325f277b3cfc26a5caa
ahchienong <ahchienong@gmail.com>
2021-06-16 00:08:23 +0800

    updated to accept new option - "update-latest-jar"

--- utilities.sh
+++ utilities.sh
@@ -15,8 +15,9 @@ service_script=${service_name}_service.sh
 echo "##############################################"
 echo "Script Name: $0 | $# arguments"
 
-if [ "$#" -ne "2" ]; then
+if [[ ("$#" -ne "1") && ("$#" -ne "2") ]]; then
         echo "----------------------------------------------"
+        echo "usage: $0 update-latest-jar"
         echo "usage: $0 update-jar <artifacts-name.jar>"
         echo "##############################################"
         exit 2
@@ -24,15 +25,31 @@ else
 
 case $1 in
        update-jar)
+        if [ "$#" -eq "1" ]; then
+                echo "insufficient info for this option"
+        fi
         NEW_JAR=$2
         if [ -f ${current_dir}/${service_script} ]; then
-                echo "updating $service_script ... latest_jar=$NEW_JAR"
+                echo "updating $service_script ..."
                 echo "latest_jar => $NEW_JAR"
                 cmd=`sed -i "0,/latest_jar/s/.*latest_jar.*/latest_jar=\"${NEW_JAR}\"/" $service_script`
                 echo $cmd
                 $cmd
         else
             echo "$service_name script not found - $service_script"
+        fi
+        ;;
+       update-latest-jar)
+        if [ -f ${current_dir}/${service_script} ] && [ -f ${current_dir}/latest_jar.list ]; then
+                echo "obtained latest_jar from file latest_jar.list"
+                NEW_JAR=$(cat latest_jar.list)
+                echo "updating $service_script ..."
+                echo "latest_jar => $NEW_JAR"
+                cmd=`sed -i "0,/latest_jar/s/.*latest_jar.*/latest_jar=\"${NEW_JAR}\"/" $service_script`
+                echo $cmd
+                $cmd
+        else
+                echo "$service_name $service_script or latest_jar.list not found"
         fi
         ;;
 esac
